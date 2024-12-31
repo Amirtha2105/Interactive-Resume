38f22a24368c3c3e22df4b77ed9d8e40ebd02872
Amirtha2105 <amirthaa2105@gmail.com>
2024-12-31 17:10:20 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+// Show pop-up details
+function showDetails(id) {
+    document.getElementById(id).style.display = 'block';
+}
+
+// Hide pop-up details
+function hideDetails(id) {
+    document.getElementById(id).style.display = 'none';
+}
+
+// Phone pop up
+function showPhonePopup() {
+    document.getElementById('phone-popup').style.display = 'block';
+}
+
+function closePhonePopup() {
+    document.getElementById('phone-popup').style.display = 'none';
+}
